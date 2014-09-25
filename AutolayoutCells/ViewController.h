73a723b136be9c96de6a75c84a433de30d6286bf
croberts <crobertsbmw@gmail.com>
2014-09-25 09:54:31 -0600

    Initial Commit

---
+++ AutolayoutCells/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutolayoutCells
+//
+//  Created by croberts on 9/25/14.
+//  Copyright (c) 2014 Roberts. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
